0363e0e9d4c088372fc2153c80384f14ac22b786
Jame3756 <75145664+Jame3756@users.noreply.github.com>
2021-08-12 14:22:13 -0700

    E

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
-import HomeScreen from "./screens/Home";
-import StartMapScreen from "./screens/StarMap";
-import DailyPicScreen from "./screens/DailyPic";
-import SpaceCraftScreen from "./screens/SpaceCraft";
+import LoginScreen from "./screens/LoginScreen";
+import LoadingScreen from "./screens/LoadingScreen";
+import DashboardScreen from "./screens/DashboardScreen";
 
-const Stack = createStackNavigator();
+import * as firebase from "firebase";
+import { firebaseConfig } from "./config";
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="StarMap" component={StartMapScreen} />
-        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
-        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
 }
 
-export default App;
+const AppSwitchNavigator = createSwitchNavigator({
+    LoadingScreen: LoadingScreen,
+    LoginScreen: LoginScreen,
+    DashboardScreen: DashboardScreen
+})
+
+const AppNavigator = createAppContainer(AppSwitchNavigator)
+
+export default function App() {
+    return (
+        <AppNavigator />
+    )
+}
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB4IhXwxnyHs8R3lzFcvYPyqJ4aL1HwLlI",
+    authDomain: "sttems-864a0.firebaseapp.com",
+    databaseURL: "https://sttems-864a0-default-rtdb.firebaseio.com",
+    projectId: "sttems-864a0",
+    storageBucket: "sttems-864a0.appspot.com",
+    messagingSenderId: "177673169272",
+    appId: "1:177673169272:web:32723c967cf88c4382c08d"
+  };
